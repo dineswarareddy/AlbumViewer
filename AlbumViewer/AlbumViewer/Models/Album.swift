b5c6062982be5dbd16e2c33e49ad6865593981f5
Dinesh Reddy Chennuru <>
2020-09-20 09:17:25 +0530

    Album model

---
+++ AlbumViewer/AlbumViewer/Models/Album.swift
@@ -0,0 +1,21 @@
+//
+//  Album.swift
+//  AlbumViewer
+//
+//  Created by Dinesh Reddy on 19/09/20.
+//
+
+import Foundation
+
+
+struct Album: Decodable {
+    let userId: Int
+    let id: Int
+    let title: String?
+    
+    internal init(userId: Int, id: Int, title: String?) {
+        self.userId = userId
+        self.id = id
+        self.title = title
+    }
+}
