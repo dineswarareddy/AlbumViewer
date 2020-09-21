78ed271c8442c6d361f0720a93ba07b633425898
Dinesh Reddy Chennuru <>
2020-09-21 19:40:38 +0530

    modified album list factory as per the updated network layer

--- AlbumViewer/AlbumViewer/Scenes/AlbumList/AlbumListViewFactory.swift
+++ AlbumViewer/AlbumViewer/Scenes/AlbumList/AlbumListViewFactory.swift
@@ -9,11 +9,11 @@ import Foundation
 
 enum AlbumListViewFactory {
     static func createAlbumsViewFactory(view: AlbumListViewPresenterOutput?) -> AlbumListViewInteractorInput {
-        let networkController = AlbumsNetworkController(requestType: .album)
+        let networkController = AlbumsNetworkController()
         let presenter = AlbumListViewPresenter(view: view)
         let interactor = AlbumListInteractor(presenter: presenter, networkController: networkController)
+        let router = AlbumListRouter()
+        presenter.router = router
         return interactor
     }
 }
-
-
